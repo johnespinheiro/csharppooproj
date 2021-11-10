0ce944f2db284441435fd064f9af4eee5f2f0052
johnespinheiro <johnes.armt@gmail.com>
2021-11-10 16:35:12 -0300

    exerc-fixacao-vetor-ok

---
+++ sec6-fixacao-vetores-71/PrimeiroProjeto/Estudante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    class Estudante
+    {
+        //declarou as duas strings, nome e email com properties get e set
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        //construtor gerado
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        //override para escrever na tela nome e email
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+    }
+}
---
+++ sec6-fixacao-vetores-71/PrimeiroProjeto/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+
+/*
+ * A dona de um pensionato possui dez quartos para alugar para estudantes,
+sendo esses quartos identificados pelos números 0 a 9.
+Quando um estudante deseja alugar um quarto, deve-se registrar o nome
+e email deste estudante.
+Fazer um programa que inicie com todos os dez quartos vazios, e depois
+leia uma quantidade N representando o número de estudantes que vão
+alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos
+N estudantes. Para cada registro de aluguel, informar o nome e email do
+estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha
+que seja escolhido um quarto vago. Ao final, seu programa deve imprimir
+um relatório de todas ocupações do pensionato, por ordem de quarto,
+conforme exemplo.
+*/
+
+namespace PrimeiroProjeto
+{
+       class Program
+    {
+        static void Main(string[] args)
+        {
+            //inicia um novo vetor (vect) estudante com 10 posições
+            Estudante[] vect = new Estudante[10];
+
+            //preenche a quant de quartos que serão alugados 
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            //laço for - inicia em 1, repete e soma 1 para cada interação até i ser igual a n
+            for (int i = 1; i <= n; i++)
+            {
+                //escreve aluguel numero:
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                //salva nome e email nas variaveis e joga pro vetor
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                //seleciona o numero do quarto e adiciona a ele as informações
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+
+
+            }
+            //relatorio quartos ocupados
+            //laço for preenchendo só os não nulos
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
