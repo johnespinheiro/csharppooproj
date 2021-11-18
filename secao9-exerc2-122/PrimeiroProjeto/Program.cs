d16411b87d755b0442319a3ec8706a8bdd6c1127
johnespinheiro <johnes.armt@gmail.com>
2021-11-18 17:49:38 -0300

    first part exerc sec 9 pg 122

---
+++ secao9-exerc2-122/PrimeiroProjeto/Program.cs
@@ -0,0 +1,18 @@
+﻿//using system - referencia para o namespace chamado system, que é padrao do .net que contem funções basicas de manipulações de programas
+using System;
+
+/*Exercício
+ * Instanciar manualmente os objetos e mostrar na tela do terminal
+ * Objetos - projeto de uma 'rede social' - cada post com seus comentarios
+ */
+namespace PrimeiroProjeto
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
